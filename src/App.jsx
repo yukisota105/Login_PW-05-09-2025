435a8ccc2221bcc5a3af646fa2d4c5d184943b70
yukisota105 <andrecamargoc18@gmail.com>
2025-09-19 22:42:40 -0300

    falta terminar 2 eventos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Evento from './components/Evento'
 import Form from './components/Form'
 import Condicional from './components/Condicional'
+
 function App() {
   return(
     <div className='App'>
--- src/components/Condicional.jsx
+++ src/components/Condicional.jsx
@@ -4,18 +4,47 @@ function Condicional(){
 
     const [email,setEmail] = useState()
     const [userEmail,setUserEmail] = useState()
+    
+    const [name,setName] = useState()
+    const [userName,setUserName] = useState()
+
+    const [senha,setP] = useState()
+    const [userSenha,setUserP] = useState()
 
 
     function enviarEmail(e){
 
         e.preventDefault()
         setUserEmail(email)
-        console.log('enviado')
+        console.log('email enviado')
     }
 
     function limparEmail(){
         setUserEmail('')
     }
+
+    function enviarName(n){
+
+        n.preventDefault()
+        setUserName(name)
+        console.log('nome inserido com sucesso')
+    }
+
+    function limparName(){
+        setUserName('')
+    }
+
+    function enviarP(s){
+
+        s.preventDefault()
+        setUserP(senha)
+        console.log('senha definida!')
+    }
+
+    function limparP(){
+        setUserP('')
+    }
+
 
     return(
         <div>
@@ -36,6 +65,45 @@ function Condicional(){
                     </div>  
                 )}
             </form>
+
+            <h2>insira seu nome </h2>
+            <form>
+                <input
+                 type="text"
+                 placeholder="insira seu nome"
+                 onChange={(n) => setName(n.target.value)}
+                 />
+                <button type="submit" onClick={enviarName}>definir nome:
+                </button>
+
+                {userName && (
+                    <div>
+                        <p>o nome do usuario é: {userName}</p>
+                        <button onClick={limparName}>Limpar Nome</button>
+                    </div>  
+                )}
+            </form>
+
+
+            <h2>insira sua senha </h2>
+            <form>
+                <input
+                 type="password"
+                 placeholder="insira sua senha"
+                 onChange={(s) => setP(s.target.value)}
+                 />
+                <button type="submit" onClick={enviarP}>insira sua senha:
+                </button>
+
+                {userSenha && (
+                    <div>
+                        <p>a senha inserida é: {userSenha}</p>
+                        <button onClick={limparP}>redefinir senha</button>
+                    </div>  
+                )}
+            </form>
+
+
         </div>
     )
 }
